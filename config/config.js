e3e52b88dc232b0d948dd93bc19c98a0e92001cc
shiggydoodah <louis@louislombardi.co.uk>
2020-04-10 21:53:51 +0100

    config js

---
+++ config/config.js
@@ -0,0 +1,7 @@
+// Copy this file as config.js in the same folder, with the proper database connection URI.
+
+module.exports = {
+  db: 'mongodb://lldesign:4HAo^dPyNjTo5FT*sy4$RW&@nXMqI#3p#@localhost:27017/test',
+  db_dev: 'mongodb://lldesign:4HAo^dPyNjTo5FT*sy4$RW&@nXMqI#3p#@localhost:27017/test'
+};
+ 
